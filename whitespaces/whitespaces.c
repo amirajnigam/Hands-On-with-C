388b7ca2bb8e98eaece2fc111b0b51e1af2b3d17
amiraj nigam <amirajnigam@gmail.com>
2020-03-13 23:23:02 -0700

    revision

--- whitespaces/whitespaces.c
+++ whitespaces/whitespaces.c
@@ -8,7 +8,7 @@ int ndigit[10];
 nwhite = nother = 0;
 for( i = 0; i < 10; ++i)
 	ndigit[i] = 0;
-while ( (c = getchar()) != '\0')
+while ( (c = getchar()) != EOF)
 {
 	if ( c >= '0' && c <= '9')
 		++ndigit[c - '0'];
@@ -19,7 +19,7 @@ while ( (c = getchar()) != '\0')
 }
 printf( " digits = ");
 for ( i = 0; i < 10; ++i)
-	printf("%d", ndigit[i]);
+	printf("%d occured %d times \n", i, ndigit[i]);
 printf(", white spaces = %d, other = %d\n", nwhite, nother);
 return (0);
 }
