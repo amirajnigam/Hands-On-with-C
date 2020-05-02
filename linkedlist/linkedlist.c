4e27f864905c0e00b1bd7c7dc3544fcc561facd5
amiraj nigam <amirajnigam@gmail.com>
2020-05-01 17:07:23 -0700

    LinkedList in Python

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -3,39 +3,39 @@
 
 struct node
 {
-	int data;
-	struct node* next;
+		int data;
+			struct node* next;
 };
 
 void	printlist(struct node* n)
 {
-	while(n != NULL)
-	{
-		printf("%d", n-> data);
-		n = n-> next;
-	}
-	putchar('\n');
+		while(n != NULL)
+				{
+							printf("%d", n-> data);
+									n = n-> next;
+										}
+			putchar('\n');
 }
 
 int main()
 {
-	struct node* first = NULL;
-	struct node* second = NULL;
-	struct node* third = NULL;
+		struct node* first = NULL;
+			struct node* second = NULL;
+				struct node* third = NULL;
 
-	first = (struct node*)malloc(sizeof(struct node));
-	second = (struct node*)malloc(sizeof(struct node));
-	third = (struct node*)malloc(sizeof(struct node));
+					first = (struct node*)malloc(sizeof(struct node));
+						second = (struct node*)malloc(sizeof(struct node));
+							third = (struct node*)malloc(sizeof(struct node));
 
-	first->data = 1;
-	first->next = second;
+								first->data = 1;
+									first->next = second;
 
-	second->data = 2;
-	second->next = third;
+										second->data = 2;
+											second->next = third;
 
-	third->data = 3;
-	third->next = NULL;
+												third->data = 3;
+													third->next = NULL;
 
-	printlist(first);
-	return (0);
+														printlist(first);
+															return (0);
 }
